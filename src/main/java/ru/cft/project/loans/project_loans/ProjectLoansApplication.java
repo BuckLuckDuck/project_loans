ec3160f9de435fba16e3658e4237827c2566b4d5
BuckLuckDuck <nikitos80011@gmail.com>
2022-07-06 21:15:20 +0700

    Initial commit

---
+++ src/main/java/ru/cft/project/loans/project_loans/ProjectLoansApplication.java
@@ -0,0 +1,13 @@
+package ru.cft.project.loans.project_loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLoansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cft/project/loans/project_loans/ProjectLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cft.project.loans.project_loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
